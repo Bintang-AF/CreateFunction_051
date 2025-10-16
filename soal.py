0934da886e8161b4d2253a4380b1b94afe17effd
Bintang-AF <bintang.aulia.ft24@mail.umy.ac.id>
2025-10-16 16:20:42 +0700

    menghitung luas jari jari

---
+++ soal.py
@@ -0,0 +1,8 @@
+luas = lambda jarijari : 3.14159 * jarijari ** 2
+
+try :
+    jari = float(input("isikan nilai jari - jari :"))
+    result = luas(jari)
+    print(F"luas dari lingkaran dengan jari jari {jari} adalah {result}")
+except ValueError :
+    print ("nilai jari - jari harus angka ")
